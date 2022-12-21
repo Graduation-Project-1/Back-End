065dcb9f689da733e63d7b567c23f41d14820e51
abdelhadymohamed1121 <abdelhadymohamed1121@gmail.com>
2022-12-21 02:34:39 +0200

    make it better

--- helper/uploadImage/uploadImage.js
+++ helper/uploadImage/uploadImage.js
@@ -1,5 +1,11 @@
 const app = require('express').Router();
+const { uploadFile, getFileStream } = require("./s3");
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
 const multer = require("multer");
+
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './uploads')
@@ -22,17 +28,26 @@ const fileFilter = (req, file, cb) => {
 
 
 const upload = multer({storage : storage, fileFilter: fileFilter,});
+
+
 const uploadImages = async(req,res)=>{
     try{
       var files = [];
-      var fileKeys = Object.keys(req.files);
-      fileKeys.forEach(function(key) {
-         files.push(req.files[key].path.replace(/\\/g, "/"));
-      }); 
-      res.status(200).json({
-         "data":files,
+      // var fileKeys = Object.keys(req.files);
+      // fileKeys.forEach(function(key) {
+      //    files.push(req.files[key].path.replace(/\\/g, "/"));
+      // });
+      req.files.map(async(item)=>{
+          await uploadFile(item).then((data)=>{
+                    files.push(data.Location);
+                    if(files.length == req.files.length){
+                        res.json({ "state": true, "message": "File Uploaded SuceesFully", Data: files});
+                    }
+          });
+          await unlinkFile(item.path);
       })
-    }catch{
+    }
+    catch{
       res.status(500).json({
         success: false,
         status: 500,
