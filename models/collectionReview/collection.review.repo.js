84f2bb8916ca74fba26c19e9d2e3447260fe1531
abdelhadymohamed1121 <abdelhadymohamed1121@gmail.com>
2023-05-27 17:54:36 +0300

    make it better

--- models/collectionReview/collection.review.repo.js
+++ models/collectionReview/collection.review.repo.js
@@ -123,6 +123,31 @@ exports.update = async (filter, query) => {
     }
 }
 
+exports.updateList = async (filter, query) => {
+    try {
+        let result = await CollectionReview.updateMany(filter, query,{new:true});
+        if (result) {
+            return {
+                success: true,
+                status: 200,
+                message: "reviewUpdated",
+            }
+        }
+        else {
+            return {
+                success: false,
+                status: 400,
+                message: "reviewNotUpdated"
+            }
+        }
+    } catch {
+        return {
+            success: false,
+            status: 500,
+            message: "some thing wrong"
+        }
+    }
+}
 
 exports.delete = async (filter) => {
     try {
